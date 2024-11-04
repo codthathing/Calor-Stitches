45a7bb0917b29dfe7082b275a4b5d5ee8dd0c65b
Uniquecon <akinwunmiolusegun277@gmail.com>
2024-11-04 23:38:46 +0100

    updated the cuatomer review

--- src/pages/body/otherpages/review/review_details.jsx
+++ src/pages/body/otherpages/review/review_details.jsx
@@ -5,7 +5,7 @@ const ReviewDetails = ({productReview, productNameText, cutOff, productPrice, av
     <div className="productDetailsDiv">
       <p className="productDetailName">{productNameText}</p>
       {(productPrice || averagePrice) && <ProductPrice priceDivClass={"start"} productPriceClass={"#FF0000"} priceClass={"productDetailPrice"} cutOff={cutOff} productPrice={productPrice} averagePrice={averagePrice} priceOne={priceOne} priceTwo={priceTwo} />}
-      {productReview && <p id="productDetailReview">(1 customer review / Add review)</p>}
+      {productReview && <p id="productDetailReview">(0 customer review / Add review)</p>}
       <p className="productDetailDesc">{productDesc}</p>
     </div>
   );

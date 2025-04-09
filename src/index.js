de16f603f933e6b54690f9408b5c694c7e12e0a9
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-04-09 19:43:47 +0100

    bug: fixed the helmet dependency error

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./style.css";
 import NavigateProvider from "./services/contexts/NavigateContext";
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <NavigateProvider>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </NavigateProvider>
   </StrictMode>
 );

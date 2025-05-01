810390453e0dcb887c48f4a5accfa07eb62da80f
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-05-01 16:21:08 +0100

    style: updated the style of the front image

--- src/components/home/HomeText.jsx
+++ src/components/home/HomeText.jsx
@@ -39,7 +39,7 @@ const HomeText = () => {
 
   return (
     <section ref={home_section} id="frontPage" >
-      <picture style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", zIndex: "-1" }}>
+      <picture style={{ position: "absolute", width: "100%", height: "100%", zIndex: "-1" }}>
         <source media="(max-width: 1023px)" srcSet={background_portrait} />
         <img src={background_landscape} alt="BACKGROUND IMAGE" loading="eager" fetchpriority ="high" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
       </picture>

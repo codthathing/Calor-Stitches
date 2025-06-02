7843c582df7a88ea8de6fada0f45e82e140786ea
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-06-02 15:35:16 +0100

    feat: updated the login button

--- src/components/header/HeaderNavigationIcons.jsx
+++ src/components/header/HeaderNavigationIcons.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect, startTransition, memo, useCallback } from "react";
+import { useContext, startTransition, memo, useCallback } from "react";
 import { NavigateContext } from "../../services/contexts/NavigateContext";
 import { FiUser, FiSearch, FiHeart, FiShoppingBag } from "react-icons/fi";
 import { motion, MotionConfig } from "framer-motion";
@@ -6,30 +6,24 @@ import { motion, MotionConfig } from "framer-motion";
 const NavigationIcons = memo(({ mount }) => {
   const { setAccount, setSearch, setWishList, setCart, cartItems, wishlistItems, navbar } = useContext(NavigateContext);
 
-  const [windowWidth, setWindowWidth] = useState(false);
-  useEffect(() => {
-    setWindowWidth(window.innerWidth < 768);
-    window.addEventListener("resize", () => setWindowWidth(window.innerWidth < 768));
-    return () => window.removeEventListener("resize", () => setWindowWidth(window.innerWidth < 768));
-  }, []);
-
   const navigationIconsDetails = [
-    { id: 0, IconClass: windowWidth ? FiUser : "loginText", text: windowWidth ? "" : "LOGIN", setPage: setAccount },
-    { id: 1, IconClass: FiSearch, setPage: setSearch },
-    { id: 2, IconClass: FiHeart, showValue: wishlistItems.length > 0, value: wishlistItems.length, setPage: setWishList },
-    { id: 3, IconClass: FiShoppingBag, showValue: cartItems.length > 0, value: cartItems.reduce((sum, { cartAmt }) => sum + cartAmt, 0), setPage: setCart },
+    { id: 0, Icon: FiUser, listId: "mobile-login-icon", setPage: setAccount },
+    { id: 1, text: "LOGIN", listIconId: "loginText", listId: "desktop-login-icon", setPage: setAccount },
+    { id: 2, Icon: FiSearch, setPage: setSearch },
+    { id: 3, Icon: FiHeart, showValue: wishlistItems.length > 0, value: wishlistItems.length, setPage: setWishList },
+    { id: 4, Icon: FiShoppingBag, showValue: cartItems.length > 0, value: cartItems.reduce((sum, { cartAmt }) => sum + cartAmt, 0), setPage: setCart },
   ];
 
   return (
     <>
-      {navigationIconsDetails.map(({ id, IconClass, showValue, value, setPage, text }) => {
+      {navigationIconsDetails.map(({ id, Icon, listId, listIconId, showValue, value, setPage, text }) => {
         return (
-          <li className="icons" onClick={() => startTransition(() => setPage(true))} key={id}>
+          <li className="icons" id={listId} onClick={() => startTransition(() => setPage(true))} key={id}>
             {text ? (
-              <span className={IconClass}>{text}</span>
+              <span id={listIconId}>{text}</span>
             ) : (
               <>
-                <IconClass className="iconTag" />
+                <Icon className="iconTag" />
                 {showValue && (
                   <motion.var className="icon-values" initial={mount && !navbar ? { ...(navbar ? { backgroundColor: "rgb(255, 255, 255)", color: "rgb(34, 34, 34)" } : { backgroundColor: "rgb(34, 34, 34)", color: "rgb(255, 255, 255)" }) } : false} animate={{ ...(navbar ? { backgroundColor: "rgb(34, 34, 34)", color: "rgb(255, 255, 255)" } : { backgroundColor: "rgb(255, 255, 255)", color: "rgb(34, 34, 34)" }) }} transition={{ type: "tween", duration: 0.35, ease: "linear" }}>
                     {value}
